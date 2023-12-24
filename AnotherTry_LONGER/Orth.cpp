1a02132b47ddce7ad9470c7fabe516003661ba9e
Cy <19179276362@163.com>
2023-12-24 23:51:45 +0800

    Add a Orth.cpp(Undone)

---
+++ AnotherTry_LONGER/Orth.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"matrix.h"
+class Orth
+{
+private:
+    matrix MAT;
+public:
+    Orth(/* args */);
+    ~Orth();
+};
+
+Orth::Orth(/* args */)
+{
+}
+
+Orth::~Orth()
+{
+}
