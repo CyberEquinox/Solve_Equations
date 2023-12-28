02bdc9bcecef0a454c3a959a6a14136026787ed2
Cy <19179276362@163.com>
2023-12-29 00:40:18 +0800

    求正负惯性指数

--- AnotherTry_LONGER/main.cpp
+++ AnotherTry_LONGER/main.cpp
@@ -17,14 +17,23 @@ int main()
     }
     generateMatrix(ID,Equations,12);
     SolveMatrix Solve(Equations);
-    Solve.SolveEquations(file,22);
+    Solve.SolveEquations(file);
     double_matrix A(Equations+Equations.Trans());
     vector<double> eigenvalues=jacobi_rotation(A,0.1);
     file<<"Eigenvalues are:"<<endl;
+    int nege=0,posi=0;
     for(double eigen:eigenvalues)
     {
         file<<eigen<<"\t";
+        if(eigen>0)
+        {
+            posi++;
+        }else if(eigen<0)
+        {
+            nege++;
+        }
     }
+    file << "\nPositive inertia index:"<<posi<<"\tNegative inertia index:"<<nege;
     file.close();
     cout<<"Output in file : \"./out.txt\"\n";
     system("pause");
